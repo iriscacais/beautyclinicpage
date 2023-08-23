2aab2de40287da3fc491de522e1b1cceae7eff07
Iris Cacais <112999212+iriscacais@users.noreply.github.com>
2023-08-23 11:51:08 -0300

    section treatments

--- beautyclinicpage/src/About/About.js
+++ beautyclinicpage/src/About/About.js
@@ -10,7 +10,7 @@ class About extends React.Component {
                 <div className="aboutImages">
                     <img src={aboutImage} className="aboutImage" alt="profissional aplicanco máscara de tratamento, e difusores e toalhas"/>
                 </div>
-                <div class="card w-50">
+                <div class="card border-0 w-50 divAb">
                     <div class="card-body">
                         <h2 class="card-title">A clínica</h2>
                         <p class="card-text aboutText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
--- beautyclinicpage/src/Treatments/Treatments.js
+++ beautyclinicpage/src/Treatments/Treatments.js
@@ -14,9 +14,9 @@ class Treatments extends React.Component {
             <section className="treatmentsSection" id="procedimemtos">
                 <h2 class="card-title">Conheça os nossos tratamentos</h2>
                 <div class="container row mx-auto divTreat">
-                    <div class="col-12 col-md-6 col-xxl-4 ">
-                        <div class="card">
-                            <img src={colageno} class="card-img-top img-thumbnail" alt="..."/>
+                    <div class="col-12 col-md-6 col-xxl-4">
+                        <div class="card border-0">
+                            <img src={colageno} class="card-img-top border-0 img-thumbnail" alt="..."/>
                                 <div class="card-body">
                                     <h5 class="card-title">Bioestimulador de colágeno</h5>
                                         <a href="#" class="btn btn-primary">Saiba Mais</a>
@@ -24,8 +24,8 @@ class Treatments extends React.Component {
                         </div>
                         </div>
                         <div class="col-12 col-md-6 col-xxl-4">
-                        <div class="card">
-                            <img src={toxinaBotulinica} class="card-img-top img-thumbnail" alt="..."/>
+                        <div class="card border-0">
+                            <img src={toxinaBotulinica} class="card-img-top border-0 img-thumbnail" alt="..."/>
                                 <div class="card-body">
                                     <h5 class="card-title">Toxina Botulínica</h5>
                                         <a href="#" class="btn btn-primary">Saiba Mais</a>
@@ -33,8 +33,8 @@ class Treatments extends React.Component {
                         </div>
                         </div>
                         <div class="col-12 col-md-6 col-xxl-4 ">
-                        <div class="card">
-                            <img src={lipoPapada} class="card-img-top img-thumbnail" alt="..."/>
+                        <div class="card border-0">
+                            <img src={lipoPapada} class="card-img-top border-0 img-thumbnail" alt="..."/>
                                 <div class="card-body">
                                     <h5 class="card-title">Lipo de papada enzimática</h5>
                                         <a href="#" class="btn btn-primary">Saiba Mais</a>
@@ -42,8 +42,8 @@ class Treatments extends React.Component {
                         </div>
                         </div>
                         <div class="col-12 col-md-6 col-xxl-4">
-                        <div class="card">
-                            <img src={enzimas} class="card-img-top img-thumbnail" alt="..."/>
+                        <div class="card border-0">
+                            <img src={enzimas} class="card-img-top border-0 img-thumbnail" alt="..."/>
                                 <div class="card-body">
                                     <h5 class="card-title">Enzimas</h5>
                                         <a href="#" class="btn btn-primary">Saiba Mais</a>
@@ -51,17 +51,17 @@ class Treatments extends React.Component {
                         </div>
                         </div>
                         <div class="col-12 col-md-6 col-xxl-4">
-                        <div class="card">
-                            <img src={capilar} class="card-img-top img-thumbnail" alt="..."/>
+                        <div class="card border-0">
+                            <img src={capilar} class="card-img-top border-0 img-thumbnail" alt="..."/>
                                 <div class="card-body">
                                     <h5 class="card-title">Tratamento capilar</h5>
                                         <a href="#" class="btn btn-primary">Saiba Mais</a>
                                 </div>
                         </div>
                         </div>
-                        <div class="col-12 col-md-6 col-xxl-4 pb-4">
-                        <div class="card">
-                            <img src={limpezaPele} class="card-img-top img-thumbnail" alt="..."/>
+                        <div class="col-12 col-md-6 col-xxl-4">
+                        <div class="card border-0">
+                            <img src={limpezaPele} class="card-img-top border-0 img-thumbnail" alt="..."/>
                                 <div class="card-body">
                                     <h5 class="card-title">Limpeza de pele</h5>
                                         <a href="#" class="btn btn-primary">Saiba Mais</a>
